440ba4723772c1313a2119d2bbb8d13752805914
Sayak Mukherjee <53646326+sayak-mukherjee8@users.noreply.github.com>
2020-12-19 18:13:33 +0530

    developing customGsearch using React

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Home from './Pages/Home.js';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import SearchPage from './Pages/SearchPage';
+
+
+function App() {
+    return (
+        <>
+            <div className='app'>
+                <Router>
+                    <Switch>
+                        <Route path='/search'>
+                            <SearchPage />
+                        </Route>
+                        <Route path='/'>
+                            <Home />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/Keys.js
@@ -0,0 +1,3 @@
+// https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "yourapikey";
+export default API_KEY;
